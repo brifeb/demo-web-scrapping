d20bdddca2a7294f2cf0b1fc982d19c147dc5574
brifeb <brifeb@gmail.com>
2021-03-01 12:47:48 +0700

    update app

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ import os
 import sys
 from flask import Flask
 
-
-sys.path.insert(0, os.path.dirname(__file__))
+#sys.path.insert(0, os.path.dirname(__file__))
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'hi ooo'
+
+if __name__ == "__main__":
+    app.run()
