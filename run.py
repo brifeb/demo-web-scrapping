9b148a3ef3ab730860262f6ecf947a234c73aedd
brifeb <brifeb@gmail.com>
2021-03-01 10:29:05 +0700

    req

--- run.py
+++ run.py
@@ -2,9 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Selamat Senin'
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+@app.route("/<string:name>/")
+def say_hello(name):
+    return f"Hello {name}!"
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
