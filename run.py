fa09fc85cb81fc87be041b82d0cfe1992326ee53
Febrianto Adi N <brifeb@gmail.com>
2021-03-01 10:41:16 +0700

    req

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
+import os
+import sys
 from flask import Flask
 
+
+sys.path.insert(0, os.path.dirname(__file__))
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hio'
-
-if __name__ == "__main__":
-    app.run()
+    return 'hi ooo'
